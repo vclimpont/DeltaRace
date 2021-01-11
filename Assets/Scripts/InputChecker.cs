b5feccf923eac20aa42d45a4e5b7b8a60918e7d7
Valentin CLIMPONT <valentin.climpont@gmail.com>
2021-01-11 15:57:47 +0100

    created input checker class
    class that checks if the player hold and/or swipe on screen

---
+++ Assets/Scripts/InputChecker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputChecker
+{
+    public bool IsDiving { get; set; }
+    public float HorizontalMovement { get; set; }
+
+    public void CheckInputs()
+    {
+        IsDiving = Input.touchCount > 0 || Input.GetMouseButton(0); // Player dives on touch / click on screen
+
+        if(IsDiving)
+        {
+            float centerX = Screen.width / 2f;
+            Vector2 touchPosition = Vector2.zero;
+
+            if(Input.touchCount > 0)
+            {
+                touchPosition = Input.GetTouch(0).position;
+            }
+            else if(Input.GetMouseButton(0))
+            {
+                touchPosition = Input.mousePosition;
+            }
+            else
+            {
+                throw new System.Exception("Player is diving with no inputs triggered");
+            }
+
+            HorizontalMovement = Mathf.Abs(touchPosition.x - centerX);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float releaseMinSpeed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float releaseSpeedFactor;
+    [SerializeField] private float diveSpeedFactor;
+
+    private Rigidbody rb;
+    private InputChecker ic;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        ic = new InputChecker();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ic.CheckInputs();
+
+        Debug.Log(ic.IsDiving + " " + ic.HorizontalMovement);
+    }
+
+    void FixedUpdate()
+    {
+        if(!ic.IsDiving)
+        {
+            Release();
+        }
+        else
+        {
+            Dive();
+            Turn(ic.HorizontalMovement);
+        }
+    }
+
+    void Release()
+    {
+
+    }
+
+    void Dive()
+    {
+
+    }
+
+    void Turn(float turnValue)
+    {
+
+    }
+}
